958489abf1bef07d62c9c98dc43de0fb4742965f
Joong-Won Seo <joong-won.seo@tum.de>
2023-01-21 23:42:56 +0100

    NOT WORKING: some basic HER

--- agent.py
+++ agent.py
@@ -291,6 +291,188 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
 
 
+
+def her(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0, 
+         steps_per_epoch=4000, epochs=100, replay_size=int(1e6), gamma=0.99, 
+         polyak=0.995, pi_lr=1e-3, q_lr=1e-3, batch_size=100, start_steps=10000, 
+         update_after=1000, update_every=50, act_noise=0.1, num_test_episodes=10, 
+         max_ep_len=1000, logger_kwargs=dict(), save_freq=1):
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    env, test_env = env_fn(), env_fn()
+    obs_dim = env.get_observation().shape[0] + env.get_goal().shape[0]
+    act_dim = env.action_dim
+
+    # Action limit for clamping: critically, assumes all dimensions share the same bound!
+    act_limit = env.action_spec[1][0]
+
+    # Create actor-critic module and target networks
+    ac = actor_critic(obs_dim, act_dim, act_limit, **ac_kwargs)
+    ac_targ = deepcopy(ac)
+
+    # Freeze target networks with respect to optimizers (only update via polyak averaging)
+    for p in ac_targ.parameters():
+        p.requires_grad = False
+
+    # Experience buffer
+    replay_buffer = ReplayBuffer(obs_dim=obs_dim, act_dim=act_dim, size=replay_size)
+
+    # Count variables (protip: try to get a feel for how different size networks behave!)
+    var_counts = tuple(core.count_vars(module) for module in [ac.pi, ac.q])
+
+    # Set up function for computing DDPG Q-loss
+    def compute_loss_q(data):
+        o, a, r, o2, d = data['obs'], data['act'], data['rew'], data['obs2'], data['done']
+
+        q = ac.q(o,a)
+
+        # Bellman backup for Q function
+        with torch.no_grad():
+            q_pi_targ = ac_targ.q(o2, ac_targ.pi(o2))
+            backup = r + gamma * (1 - d) * q_pi_targ
+
+        # MSE loss against Bellman backup
+        loss_q = ((q - backup)**2).mean()
+
+        # Useful info for logging
+        loss_info = dict(QVals=q.detach().numpy())
+
+        return loss_q, loss_info
+
+    # Set up function for computing DDPG pi loss
+    def compute_loss_pi(data):
+        o = data['obs']
+        q_pi = ac.q(o, ac.pi(o))
+        return -q_pi.mean()
+
+    # Set up optimizers for policy and q-function
+    pi_optimizer = Adam(ac.pi.parameters(), lr=pi_lr)
+    q_optimizer = Adam(ac.q.parameters(), lr=q_lr)
+
+    # Set up model saving
+
+    def update(data):
+        # First run one gradient descent step for Q.
+        q_optimizer.zero_grad()
+        loss_q, loss_info = compute_loss_q(data)
+        loss_q.backward()
+        q_optimizer.step()
+
+        # Freeze Q-network so you don't waste computational effort 
+        # computing gradients for it during the policy learning step.
+        for p in ac.q.parameters():
+            p.requires_grad = False
+
+        # Next run one gradient descent step for pi.
+        pi_optimizer.zero_grad()
+        loss_pi = compute_loss_pi(data)
+        loss_pi.backward()
+        pi_optimizer.step()
+
+        # Unfreeze Q-network so you can optimize it at next DDPG step.
+        for p in ac.q.parameters():
+            p.requires_grad = True
+
+        # Record things
+
+        # Finally, update target networks by polyak averaging.
+        with torch.no_grad():
+            for p, p_targ in zip(ac.parameters(), ac_targ.parameters()):
+                # NB: We use an in-place operations "mul_", "add_" to update target
+                # params, as opposed to "mul" and "add", which would make new tensors.
+                p_targ.data.mul_(polyak)
+                p_targ.data.add_((1 - polyak) * p.data)
+
+    def get_action(o, noise_scale):
+        a = ac.act(torch.as_tensor(o, dtype=torch.float32))
+        a += noise_scale * np.random.randn(act_dim)
+        return np.clip(a, -act_limit, act_limit)
+
+    def test_agent():
+        for j in range(num_test_episodes):
+            o, d, ep_ret, ep_len = test_env.reset(), False, 0, 0
+            while not(d or (ep_len == max_ep_len)):
+                # Take deterministic actions at test time (noise_scale=0)
+                o, r, d, _ = test_env.step(get_action(o, 0))
+                ep_ret += r
+                ep_len += 1
+            print('ep_ret: ', ep_ret)
+
+    # Prepare for interaction with environment
+    total_steps = steps_per_epoch * epochs
+    start_time = time.time()
+    o, ep_ret, ep_len = env.reset(), 0, 0
+    g = env.get_task_goal(o)
+    og = np.concatenate((o, g))
+
+    # Main loop: collect experience in env and update/log each epoch
+    # TODO: restructure to for epochs for episodes for steps
+    for t in range(total_steps):
+        # Until start_steps have elapsed, randomly sample actions
+        # from a uniform distribution for better exploration. Afterwards, 
+        # use the learned policy (with some noise, via act_noise). 
+        if t > start_steps:
+            a = get_action(og, act_noise)
+        else:
+            a = random_action(env)
+
+        # Step the env
+        o2, r, d, _ = env.step(a)
+        og2 = np.concatenate((o2, g))
+        r = env.get_reward(o2, g)
+        ep_ret += r
+        ep_len += 1
+
+        # Ignore the "done" signal if it comes from hitting the time
+        # horizon (that is, when it's an artificial terminal signal
+        # that isn't based on the agent's state)
+        d = False if ep_len==max_ep_len else d
+
+        # Store experience to replay buffer
+        replay_buffer.store(og, a, r, og2, d)
+
+        # Super critical, easy to overlook step: make sure to update 
+        # most recent observation!
+        o = o2
+
+        # End of trajectory handling
+        if d or (ep_len == max_ep_len):
+            # HER: generate new virtual goals and store it in replay buffer
+            # final strategy: last state of episode becomes the goal
+            virtual_goal = env.as_goal(o)
+            og_virtual = np.concatenate((o, virtual_goal))
+            
+            # TODO: wrong. instead, resave the entire trajectory with the new virtual goal
+            replay_buffer.store(og_virtual, a, env.get_reward(o, virtual_goal), og2, d)
+
+            o, ep_ret, ep_len = env.reset(), 0, 0
+            g = env.get_task_goal(o)
+            og = np.concatenate((o, g))
+
+        # Update handling
+        if t >= update_after and t % update_every == 0:
+            for _ in range(update_every):
+                batch = replay_buffer.sample_batch(batch_size)
+                update(data=batch)
+
+        # End of epoch handling
+        if (t+1) % steps_per_epoch == 0:
+            epoch = (t+1) // steps_per_epoch
+
+            # Test the performance of the deterministic version of the agent.
+            test_agent()
+
+            # Save model
+            torch.save(ac.state_dict(), 'weights/agent.pth')
+
+            # Progess bar
+            print(('#' * round(epoch/epochs * 100)).ljust(100, '-') + f"  reward={ep_ret}", end='\n')
+    print('\ndone')
+
+
+
             
 
 if __name__ == '__main__':
@@ -319,6 +501,28 @@ if __name__ == '__main__':
                 obs = env._get_observations()
             return np.concatenate([obs['object-state'], obs['robot0_proprio-state']])
         env.get_observation = get_observation
+
+        # create a initial state to task goal mapper, specific to the task
+        def get_task_goal(obs=None):
+            if obs is None:
+                obs = env._get_observations()
+            goal = obs['object-state'][:3]
+            goal[2] += 0.05 # lift the object by 5cm (in the lift task, it's defined as 4cm above table height)
+            return goal
+        env.get_task_goal = get_task_goal
+
+        # create a state to goal mapper for HER, such that the input state safisfies the returned goal
+        def as_goal(obs=None):
+            if obs is None:
+                obs = env._get_observations()
+            return obs['object-state'][:3] # object position
+        env.get_goal = as_goal
+
+        # create a state-goal to reward function (sparse)
+        def get_reward(obs, goal):
+            # as long as the object is close enough to the goal, the reward is 1
+            return 1 if np.linalg.norm(obs['object-state'][:3] - goal) < 0.05 else 0
+        env.get_reward = get_reward
 
         # wrap the step function to return the wrapped observation
         env._step = env.step
