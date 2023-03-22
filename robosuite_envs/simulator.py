09c4205187d4d768d033c1488c79eb4eb2a9f797
Joong-Won Seo <joong-won.seo@tum.de>
2023-03-22 21:45:08 +0100

    EXAMPLE set state and render without step

--- robosuite_envs/simulator.py
+++ robosuite_envs/simulator.py
@@ -26,7 +26,6 @@ assert all(-env.action_space.low == env.action_space.high)
 agent_action_limit = env.action_space.high
 
 agent = core.MLPActorCritic(agent_input_dim, agent_output_dim, agent_action_limit)
-# agent.load_state_dict(torch.load('../rl/weights/agent_succ.pth'))
 
 
 # simulation
@@ -36,29 +35,14 @@ def main():
         obs, info = env.reset()
         obs = np.concatenate((obs['observation'], obs['desired_goal']))
 
-        total_reward = 0
         for t in range(horizon):
-            
-            # DEBUG move the goal around
-            # env.episode_goal[0] += 0.01 * (env.renderer.is_pressed('l') - env.renderer.is_pressed('j'))
-            # env.episode_goal[1] += 0.01 * (env.renderer.is_pressed('i') - env.renderer.is_pressed('k'))
-            # env.episode_goal[2] += 0.01 * (env.renderer.is_pressed('9') - env.renderer.is_pressed(','))
-            # set_obj_po(env.robo_env.sim, joint='cube_joint0')
+            # randomize env
+            set_obj_pos(env.robo_env.sim, joint='cube_joint0')
+            env.robo_env.robots[0].set_robot_joint_positions(np.random.randn(7))
+            # render
+            obs = env.robo_env._get_observations(force_update=True)
+            env.render_frame(obs, info)
 
-            # Simulation
-            # action = agent.noisy_action(obs, 0) # sample agent action
-            action = np.random.randn(agent_output_dim) # sample random action
-            obs, reward, terminated, truncated, info = env.step(action)  # take action in the environment
-            obs = np.concatenate((obs['observation'], obs['desired_goal']))
-
-            total_reward += reward
-            if info['is_success']:
-                print('s', end='')
-
-            if terminated or truncated:
-                break
-    
-        print(f"\ntotal_reward = {total_reward}")
 
 
 if __name__ == '__main__':
